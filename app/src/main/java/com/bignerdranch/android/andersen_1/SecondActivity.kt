3a6d753cdc0ca400231192a1a0b1df6a3c14cdc6
Maksim Omelchenko <m.omelchenko@qa-academy.by>
2021-10-02 10:34:20 +0300

    commit with added logic for reply text in TwoActivitiesAndIntents.kt and SecondActivity.kt

--- app/src/main/java/com/bignerdranch/android/andersen_1/SecondActivity.kt
+++ app/src/main/java/com/bignerdranch/android/andersen_1/SecondActivity.kt
@@ -4,20 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+
+const val EXTRA_MESSAGE_SECOND_ACTIVITY: String =
+    "com.big_nerd_ranch.android.andersen_1.second_activity.extra.MESSAGE"
+const val EXTRA_REPLY: String =
+    "com.big_nerd_ranch.android.andersen_1.twoactivities.extra.REPLY";
+lateinit var replyToFirst: String
 
 class SecondActivity : AppCompatActivity() {
 
     private val tagSecondActivity: String = SecondActivity::class.java.name
+    private lateinit var messageSendReply: EditText
+    private lateinit var buttonLaunchPreviousActivity: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         Log.d(tagSecondActivity, getString(R.string.funny_log_dad))
-
         val intent: Intent = intent
         val message = intent.getStringExtra(EXTRA_MESSAGE)
         val textView: TextView = findViewById(R.id.text_message)
         textView.text = message
+
+        messageSendReply = findViewById(R.id.text_for_reply)
+
+        buttonLaunchPreviousActivity = findViewById(R.id.button_to_first_activity)
+        buttonLaunchPreviousActivity.setOnClickListener(::sendRequestedResult)
+    }
+
+    private fun sendRequestedResult(view: View) {
+        val intent = Intent(applicationContext, TwoActivitiesAndIntents::class.java)
+        intent.putExtra(EXTRA_MESSAGE_SECOND_ACTIVITY, messageSendReply.text.toString())
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
--- app/src/main/java/com/bignerdranch/android/andersen_1/TwoActivitiesAndIntents.kt
+++ app/src/main/java/com/bignerdranch/android/andersen_1/TwoActivitiesAndIntents.kt
@@ -7,32 +7,55 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 const val EXTRA_MESSAGE: String =
     "com.big_nerd_ranch.android.andersen_1.two_activities.extra.MESSAGE"
+const val TEXT_REQUEST = 1
 
 class TwoActivitiesAndIntents : AppCompatActivity() {
 
     private val tagTwoActivitiesAndIntents: String = TwoActivitiesAndIntents::class.java.name
     private lateinit var messageEditText: EditText
     private lateinit var buttonLaunchSecondActivity: Button
+    private lateinit var textViewRepliedMessage: TextView
+    private lateinit var textViewHeader: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_two_activities_and_intents)
         Log.d(tagTwoActivitiesAndIntents, getString(R.string.funny_log_dad))
 
-        messageEditText = findViewById(R.id.editText_main)
+        val intent: Intent = intent
+        val message = intent.getStringExtra(EXTRA_MESSAGE_SECOND_ACTIVITY)
+
+        messageEditText = findViewById(R.id.message_for_sent_to_second)
+
+        textViewRepliedMessage = findViewById(R.id.text_message_replied)
+        textViewRepliedMessage.text = message
+
+        textViewHeader = findViewById(R.id.text_header)
 
         buttonLaunchSecondActivity = findViewById(R.id.button_to_second_activity)
         buttonLaunchSecondActivity.setOnClickListener(::launchSecondActivity)
     }
 
     private fun launchSecondActivity(view: View) {
-        Log.d(tagTwoActivitiesAndIntents, "Button Clicked")
+        Log.d(tagTwoActivitiesAndIntents, getString(R.string.log_what_happend_with_button))
         val intent = Intent(this, SecondActivity::class.java)
         val message: String = messageEditText.text.toString()
         intent.putExtra(EXTRA_MESSAGE, message)
-        startActivity(intent)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val reply = data?.getStringExtra(EXTRA_MESSAGE_SECOND_ACTIVITY)
+                textViewRepliedMessage.text = reply
+                textViewHeader.visibility = View.VISIBLE
+            }
+        }
     }
 }
